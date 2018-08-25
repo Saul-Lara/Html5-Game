a50855a2a134a401626b1321806db77c136decd3
Saul-Lara <33563111+Saul-Lara@users.noreply.github.com>
2018-08-25 00:11:28 -0500

    Load the file specified in script

--- scripts/jewel.js
+++ scripts/jewel.js
@@ -1,7 +1,34 @@
 var jewel = (function() {
+	var scriptQueue = [],
+		numResourcesLoaded = 0,
+		numResources = 0,
+		executeRunning = false;
+
+	function executeScriptQueue() {
+		
+	}
 
 	function load(src, callback){
+		var image, queueEntry;
+		numResources++;
 
+		queueEntry = {	// Add this resource to the execution queue
+			src: src,
+			callback: callback,
+			loaded: false
+		};
+
+		scriptQueue.push(queueEntry);
+
+		image = new Image();
+		image.onload = image.onerror = function () {
+			numResourcesLoaded++;
+			queueEntry.loaded = true;
+			if(!executeRunning){
+				executeScriptQueue();
+			}
+		};
+		image.src = src;
 	}
 
 	function setup() {
