c15edf1c2446353026913fcd4c11d4b253d14de7
Saul Hdz Lara <knighthdz97@gmail.com>
2021-06-15 02:28:31 -0500

    :construction: UPDATE: Add message handler
    - Add message handler function for worker

--- scripts/board.worker-interface.js
+++ scripts/board.worker-interface.js
@@ -2,9 +2,15 @@ jewel.board = (function () {
 	var worker, messageCount, callbacks;
 
 	function initialize(callback) {
+		rows = jewel.settings.rows;
+		cols = jewel.settings.cols;
+
 		messageCount = 0;
 		callbacks = [];
 		worker = new Worker("scripts/board.worker.js");
+
+		jewel.dom.bind(worker, "message", messageHandler);
+		post("initialize", jewel.settings, callback);
 	}
 
 	function post(command, data, callback) {
@@ -16,4 +22,57 @@ jewel.board = (function () {
 		});
 		messageCount++;
 	}
+
+	function messageHandler(event) {
+		// Uncomment to log worker messages
+		// console.log(event.data);
+
+		var message = event.data;
+		jewels = message.jewels;
+
+		if (callbacks[message.id]) {
+			callbacks[message.id](message.data);
+			delete callbacks[message.id];
+		}
+	}
+
+	function swap(x1, y1, x2, y2, callback) {
+		post("swap", { x1: x1, y1: y1, x2: x2, y2: y2 }, callback);
+	}
+
+	function getBoard() {
+		var copy = [];
+
+		for (var x = 0; x < cols; x++) {
+			copy[x] = jewels[x].slice(0);
+		}
+
+		return copy;
+	}
+
+	function print() {
+		var str = "";
+		for (var y = 0; y < rows; y++) {
+			for (var x = 0; x < cols; x++) {
+				str += getJewel(x, y) + " ";
+			}
+			str += "\r\n";
+		}
+		console.log(str);
+	}
+
+	function getJewel(x, y) {
+		if (x < 0 || x > cols - 1 || y < 0 || y > rows - 1) {
+			return -1;
+		} else {
+			return jewels[x][y];
+		}
+	}
+
+	return {
+		initialize: initialize,
+		swap: swap,
+		getBoard: getBoard,
+		print: print,
+	};
 })();
