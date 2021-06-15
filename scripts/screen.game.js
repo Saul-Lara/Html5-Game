40705856db90281de038e5daf8a1e91ba205b0c6
Saul Hdz Lara <knighthdz97@gmail.com>
2021-06-15 18:36:11 -0500

    :sparkles: NEW: Add pause and exit functionalities

--- scripts/screen.game.js
+++ scripts/screen.game.js
@@ -1,5 +1,6 @@
 jewel.screens["game-screen"] = (function () {
-	var firstRun = true;
+	var firstRun = true,
+		paused;
 
 	function startGame() {
 		var board = jewel.board,
@@ -12,10 +13,46 @@ jewel.screens["game-screen"] = (function () {
 				});
 			});
 		});
+
+		paused = false;
+		var dom = jewel.dom,
+			overlay = dom.$("#game-screen .pause-overlay")[0];
+		overlay.style.display = "none";
+	}
+
+	function exitGame() {
+		pauseGame();
+		var confirmed = window.confirm("Do you want to return to the main menu?");
+		if (confirmed) {
+			jewel.showScreen("main-menu");
+		} else {
+			resumeGame();
+		}
+	}
+
+	function pauseGame() {
+		if (paused) {
+			return; // Do nothing if already paused
+		}
+		var dom = jewel.dom,
+			overlay = dom.$("#game-screen .pause-overlay")[0];
+		overlay.style.display = "block";
+		paused = true;
+	}
+
+	function resumeGame() {
+		var dom = jewel.dom,
+			overlay = dom.$("#game-screen .pause-overlay")[0];
+		overlay.style.display = "none";
+		paused = false;
 	}
 
 	function setup() {
-		// Do nothing for now
+		var dom = jewel.dom;
+
+		dom.bind("footer button.exit", "click", exitGame);
+		dom.bind("footer button.pause", "click", pauseGame);
+		dom.bind(".pause-overlay", "click", resumeGame);
 	}
 
 	function run() {
